8b394762ec5753674641cdcb90d675d86c7c327b
le.tong <le.tong@ecovacs.com>
2021-07-12 17:02:27 +0800

    Initial Commit

---
+++ Tong-C/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Tong-C
+//
+//  Created by Tongle on 2021/7/12.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
